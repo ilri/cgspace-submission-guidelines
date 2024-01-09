1beb694deb3c2a80ccdeeb6e8bd41b8eef3d7ad8
Alan Orth <alan.orth@gmail.com>
2024-01-09 23:52:28 +0300

    util/parse-input-forms.py: update for DSpace 7 forms

--- util/parse-input-forms.py
+++ util/parse-input-forms.py
@@ -65,132 +65,133 @@ def getFieldDescription(schema: str, element: str, qualifier: str) -> str:
 
 
 def parseInputForm(inputForm):
-    # Get all fields on page 1 of the default DSpace submission form
-    submissionFormFields = inputForm.findall(
-        "./form-definitions/form[@name='traditional']/page[@number='1']/field"
-    )
+    # Get all fields from the default DSpace submission form (pages 1-3)
+    for page in ["one", "two", "three"]:
+        submissionFormFields = inputForm.findall(
+            f".//form-definitions/form[@name='traditionalpage{page}']//field"
+        )
 
-    for field in submissionFormFields:
-        # Initialize all variables with every new iteration of the for loop so
-        # we are sure we are getting values for the current field.
-        schema = None
-        element = None
-        qualifier = None
-        metadataField = None
-        metadataFieldSlug = None
-        repeatable = None
-        title = None
-        vocabulary = None
-        inputType = None
-        policy = None
-        valuePairs = None
-        description = None
-        required = None
+        for field in submissionFormFields:
+            # Initialize all variables with every new iteration of the for loop so
+            # we are sure we are getting values for the current field.
+            schema = None
+            element = None
+            qualifier = None
+            metadataField = None
+            metadataFieldSlug = None
+            repeatable = None
+            title = None
+            vocabulary = None
+            inputType = None
+            policy = None
+            valuePairs = None
+            description = None
+            required = None
 
-        schema = field.find("./dc-schema").text
-        element = field.find("./dc-element").text
+            schema = field.find("./dc-schema").text
+            element = field.find("./dc-element").text
 
-        # Not all fields have qualifiers so we need to catch the AttributeError
-        try:
-            qualifier = field.find("./dc-qualifier").text
-        except AttributeError:
-            qualifier = False
-
-        if qualifier:
-            metadataField = f"{schema}.{element}.{qualifier}"
-            metadataFieldSlug = f"{schema}-{element}-{qualifier}"
-        else:
-            metadataField = f"{schema}.{element}"
-            metadataFieldSlug = f"{schema}-{element}"
-
-        print(f"Processing {metadataField}")
-
-        # Create output directory for term
-        outputDirectory = f"content/terms/{metadataFieldSlug}"
-        os.makedirs(outputDirectory, mode=0o755, exist_ok=True)
-
-        repeatable = field.find("./repeatable").text
-        title = field.find("./label").text
-
-        # Check if this field uses an external controlled vocabulary
-        try:
-            vocabulary = field.find("./vocabulary").text
-        except AttributeError:
-            vocabulary = False
-
-        if vocabulary:
-            exportControlledVocabularies(vocabulary, metadataFieldSlug)
-
-        # Not using this yet, but could eventually say that the field is free
-        # text if input type is onebox, or controlled if value is dropdown.
-        inputType = field.find("./input-type").text
-
-        # If this field uses DSpace value pairs it will have an attribute named
-        # "value-pairs-name".
-        valuePairs = field.find("./input-type[@value-pairs-name]")
-        # If value-pairs-name is present we can then get the name of the value
-        # pairs being used for this field.
-        if valuePairs is not None:
-            # find and findall only implement a subset of XPath so we can't use
-            # field.find("./input-type/@value-pairs-name"). Use attrib...
-            #
-            # See: https://stackoverflow.com/a/6126846
-            valuePairsName = valuePairs.attrib["value-pairs-name"]
-
-            exportValuePairs(root, valuePairsName, metadataFieldSlug)
-
-        # Try to infer some sort of policy about the field based on the type of
-        # input and whether it uses value-pairs or a vocabulary.
-        if inputType == "onebox" and not vocabulary:
-            policy = "Free text."
-        elif inputType == "onebox" and vocabulary:
-            policy = "Free text, with suggested values from vocabulary."
-        elif inputType == "dropdown" and valuePairs is not None:
-            policy = "Controlled, with values from vocabulary."
-        elif inputType == "twobox" or inputType == "textarea":
-            policy = "Free text."
-
-        # Try to get the metadata field's description from the REST API
-        description = getFieldDescription(schema, element, qualifier)
-        # We can theoretically fall back to the "hint" in the input-forms.xml,
-        # but they are really meant for editors, not developers.
-        if description is None or description == "":
+            # Not all fields have qualifiers so we need to catch the AttributeError
             try:
-                description = field.find("./hint").text
+                qualifier = field.find("./dc-qualifier").text
             except AttributeError:
-                description = ""
-        else:
-            # Delete some unneeded comments from the metadata description. I do
-            # this here as a bit of a hack, because I want to include some link
-            # to a vocab in the metadata registry, but I don't want them on the
-            # documentation page.
-            description = re.sub(r"\sSee.*?\.txt$", "", description)
+                qualifier = False
 
-        if field.find("./required").text:
-            required = True
-        else:
-            required = False
+            if qualifier:
+                metadataField = f"{schema}.{element}.{qualifier}"
+                metadataFieldSlug = f"{schema}-{element}-{qualifier}"
+            else:
+                metadataField = f"{schema}.{element}"
+                metadataFieldSlug = f"{schema}-{element}"
 
-        # Create an empty list with lines we'll write to the term's index.md in
-        # TOML frontmatter format for Hugo.
-        indexLines = []
-        indexLines.append("---\n")
-        indexLines.append(f"title: '{title}'\n")
-        indexLines.append(f"field: '{metadataField}'\n")
-        indexLines.append(f"slug: '{metadataFieldSlug}'\n")
-        if description != title:
-            indexLines.append(f"description: '{description}'\n")
-        indexLines.append(f"required: {required}\n")
-        if vocabulary or valuePairs is not None:
-            indexLines.append(f"vocabulary: '{metadataFieldSlug}.txt'\n")
-        if policy is not None:
-            indexLines.append(f"policy: '{policy}'\n")
-        # TODO: use some real date...?
-        indexLines.append("date: '2019-05-04T00:00:00+00:00'\n")
-        indexLines.append("---")
+            print(f"Processing {metadataField}")
 
-        with open(f"content/terms/{metadataFieldSlug}/index.md", "w") as f:
-            f.writelines(indexLines)
+            # Create output directory for term
+            outputDirectory = f"content/terms/{metadataFieldSlug}"
+            os.makedirs(outputDirectory, mode=0o755, exist_ok=True)
+
+            repeatable = field.find("./repeatable").text
+            title = field.find("./label").text
+
+            # Check if this field uses an external controlled vocabulary
+            try:
+                vocabulary = field.find("./vocabulary").text
+            except AttributeError:
+                vocabulary = False
+
+            if vocabulary:
+                exportControlledVocabularies(vocabulary, metadataFieldSlug)
+
+            # Not using this yet, but could eventually say that the field is free
+            # text if input type is onebox, or controlled if value is dropdown.
+            inputType = field.find("./input-type").text
+
+            # If this field uses DSpace value pairs it will have an attribute named
+            # "value-pairs-name".
+            valuePairs = field.find("./input-type[@value-pairs-name]")
+            # If value-pairs-name is present we can then get the name of the value
+            # pairs being used for this field.
+            if valuePairs is not None:
+                # find and findall only implement a subset of XPath so we can't use
+                # field.find("./input-type/@value-pairs-name"). Use attrib...
+                #
+                # See: https://stackoverflow.com/a/6126846
+                valuePairsName = valuePairs.attrib["value-pairs-name"]
+
+                exportValuePairs(root, valuePairsName, metadataFieldSlug)
+
+            # Try to infer some sort of policy about the field based on the type of
+            # input and whether it uses value-pairs or a vocabulary.
+            if inputType == "onebox" and not vocabulary:
+                policy = "Free text."
+            elif inputType == "onebox" and vocabulary:
+                policy = "Free text, with suggested values from vocabulary."
+            elif inputType == "dropdown" and valuePairs is not None:
+                policy = "Controlled, with values from vocabulary."
+            elif inputType == "twobox" or inputType == "textarea":
+                policy = "Free text."
+
+            # Try to get the metadata field's description from the REST API
+            description = getFieldDescription(schema, element, qualifier)
+            # We can theoretically fall back to the "hint" in the input-forms.xml,
+            # but they are really meant for editors, not developers.
+            if description is None or description == "":
+                try:
+                    description = field.find("./hint").text
+                except AttributeError:
+                    description = ""
+            else:
+                # Delete some unneeded comments from the metadata description. I do
+                # this here as a bit of a hack, because I want to include some link
+                # to a vocab in the metadata registry, but I don't want them on the
+                # documentation page.
+                description = re.sub(r"\sSee.*?\.txt$", "", description)
+
+            if field.find("./required").text:
+                required = True
+            else:
+                required = False
+
+            # Create an empty list with lines we'll write to the term's index.md in
+            # TOML frontmatter format for Hugo.
+            indexLines = []
+            indexLines.append("---\n")
+            indexLines.append(f"title: '{title}'\n")
+            indexLines.append(f"field: '{metadataField}'\n")
+            indexLines.append(f"slug: '{metadataFieldSlug}'\n")
+            if description != title:
+                indexLines.append(f"description: '{description}'\n")
+            indexLines.append(f"required: {required}\n")
+            if vocabulary or valuePairs is not None:
+                indexLines.append(f"vocabulary: '{metadataFieldSlug}.txt'\n")
+            if policy is not None:
+                indexLines.append(f"policy: '{policy}'\n")
+            # TODO: use some real date...?
+            indexLines.append("date: '2019-05-04T00:00:00+00:00'\n")
+            indexLines.append("---")
+
+            with open(f"content/terms/{metadataFieldSlug}/index.md", "w") as f:
+                f.writelines(indexLines)
 
 
 def exportValuePairs(inputFormsXmlRoot, valuePairsName: str, metadataFieldSlug: str):
